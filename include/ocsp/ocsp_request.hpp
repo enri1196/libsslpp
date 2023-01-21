0966d321101d995dd86077a845d65cb529a628e0
Enri1196 <enrico1196@gmail.com>
2023-01-21 17:13:25 +0100

    init builder, set_name, sign methods

--- include/ocsp/ocsp_request.hpp
+++ include/ocsp/ocsp_request.hpp
@@ -6,9 +6,15 @@
 
 #include <openssl/ocsp.h>
 
+#include "evp_pkey.hpp"
 #include "internal/ssl_interface.hpp"
+#include "openssl/evp.h"
+#include "x509/x509_certificate.hpp"
+#include "x509/x509_name.hpp"
 
-namespace openssl {
+namespace openssl::ocsp {
+
+class OCSPRequestBuilder;
 
 class OCSPRequest {
 private:
@@ -29,6 +35,12 @@ public:
 
   auto as_ptr() const noexcept -> OCSP_REQUEST* { return m_ssl_type.get(); }
 
+  template <class Builder>
+    requires std::is_same_v<Builder, OCSPRequestBuilder>
+  static auto init() -> Builder {
+    return Builder();
+  }
+
   static auto from(const std::vector<std::uint8_t>&& bytes) -> Expected<OCSPRequest> {
     auto bytes_data = bytes.data();
     auto req = d2i_OCSP_REQUEST(nullptr, &bytes_data, bytes.size());
@@ -44,6 +56,12 @@ private:
   OCSP_REQUEST* req{OCSP_REQUEST_new()};
 
 public:
+  OCSPRequestBuilder() = delete;
+  OCSPRequestBuilder(const OCSPRequestBuilder &) = delete;
+  OCSPRequestBuilder(OCSPRequestBuilder &&) noexcept = default;
+  auto operator=(const OCSPRequestBuilder &) -> OCSPRequestBuilder & = delete;
+  auto operator=(OCSPRequestBuilder &&) noexcept -> OCSPRequestBuilder & = default;
+
   auto set_nonce(const std::vector<std::uint8_t>&& bytes) -> OCSPRequestBuilder {
     OCSP_request_add1_nonce(req, const_cast<unsigned char*>(bytes.data()), static_cast<int>(bytes.size()));
     return std::forward<OCSPRequestBuilder>(*this);
@@ -56,6 +74,16 @@ public:
 
   auto set_issuer(X509 *issuer) -> OCSPRequestBuilder {
     OCSP_request_add1_cert(req, issuer);
+    return std::forward<OCSPRequestBuilder>(*this);
+  }
+
+  auto set_name(const X509Name&& name) -> OCSPRequestBuilder {
+    OCSP_request_set1_name(req, name.as_ptr());
+    return std::forward<OCSPRequestBuilder>(*this);
+  }
+
+  auto sign(const EVPPkey<Private> pkey, const X509Certificate&& signer, const EVP_MD* dgst = EVP_sha256()) -> OCSPRequestBuilder {
+    OCSP_request_sign(req, signer.as_ptr(), pkey.as_ptr(), dgst, nullptr, 0);
     return std::forward<OCSPRequestBuilder>(*this);
   }
 
