473add8dbce763108adfd63dcd02eaf99f0ee9b0
Enri1196 <enrico1196@gmail.com>
2023-01-30 23:23:28 +0100

    X509Name

--- include/x509/x509_name.hpp
+++ include/x509/x509_name.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstdint>
 #include <memory>
 #include <string_view>
 
@@ -9,6 +10,8 @@
 #include "internal/ssl_interface.hpp"
 
 namespace openssl {
+
+class X509NameBuilder;
 
 class X509Name {
 private:
@@ -29,12 +32,95 @@ public:
 
   auto as_ptr() const noexcept -> X509_NAME * { return m_ssl_type.get(); }
 
+  template <class Builder = X509NameBuilder>
+    requires std::is_same_v<Builder, X509NameBuilder>
+  static auto init() -> Builder {
+    return Builder();
+  }
+
   auto to_string() -> Expected<std::string_view> {
     auto bio = SSLBio::init();
-    auto p =
-        X509Name(X509_NAME_new(), [](X509_NAME *ptr) { X509_NAME_free(ptr); });
-    X509_NAME_print_ex(bio.as_ptr(), this->as_ptr(), 0, XN_FLAG_ONELINE);
+    X509_NAME_print_ex(bio.as_ptr(), this->as_ptr(), 0, XN_FLAG_RFC2253);
     return bio.get_mem_ptr();
+  }
+};
+
+class X509NameEntry {
+public:
+  enum class entries {
+    C,
+    CN,
+    DC,
+    Email,
+    GivenName,
+    L,
+    O,
+    OU,
+    SN,
+    ST,
+    Surname,
+    UID,
+  };
+
+private:
+  entries m_ent;
+
+public:
+  X509NameEntry() = delete;
+  explicit X509NameEntry(entries ent) : m_ent(ent) {}
+
+  auto to_string() const -> std::string_view {
+    switch (m_ent) {
+    case entries::C:
+      return "C";
+    case entries::CN:
+      return "CN";
+    case entries::DC:
+      return "DC";
+    case entries::Email:
+      return "Email";
+    case entries::GivenName:
+      return "GivenName";
+    case entries::L:
+      return "L";
+    case entries::O:
+      return "O";
+    case entries::OU:
+      return "OU";
+    case entries::SN:
+      return "SN";
+    case entries::ST:
+      return "ST";
+    case entries::Surname:
+      return "Surname";
+    case entries::UID:
+      return "UID";
+    }
+  }
+};
+
+class X509NameBuilder {
+private:
+  X509_NAME* name{X509_NAME_new()};
+
+  friend X509Name;
+
+  X509NameBuilder() = default;
+
+public:
+  X509NameBuilder(const X509NameBuilder &) = delete;
+  X509NameBuilder(X509NameBuilder &&) noexcept = default;
+  auto operator=(const X509NameBuilder &) -> X509NameBuilder & = delete;
+  auto operator=(X509NameBuilder &&) noexcept -> X509NameBuilder & = default;
+
+  auto add_entry(const X509NameEntry&& entry, const std::string_view&& value) -> X509NameBuilder {
+    auto field = entry.to_string().data();
+    X509_NAME_add_entry_by_txt(name, field, MBSTRING_ASC, reinterpret_cast<const std::uint8_t*>(value.data()), -1, -1, 0);
+    return std::forward<X509NameBuilder>(*this);
+  }
+
+  auto build() -> X509Name {
+    return X509Name(name);
   }
 };
 
