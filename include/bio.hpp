ddc1be5470fa86551c8129a561735e85925c0326
Enri1196 <enrico1196@gmail.com>
2023-04-26 20:47:21 +0200

    bio openssl ref count

--- include/bio.hpp
+++ include/bio.hpp
@@ -3,58 +3,36 @@
 #include <filesystem>
 
 #include <openssl/bio.h>
-#include <openssl/ssl.h>
+#include <openssl/buffer.h>
 
 #include "internal/ssl_interface.hpp"
 
 namespace openssl {
 
-class SSLBio {
+class LIBSSLPP_PUBLIC SSLBio {
 private:
-  using SSLPtr = std::shared_ptr<BIO>;
-  SSLPtr m_ssl_type;
+  BIO* m_ssl_type;
 
-  explicit SSLBio(const BIO_METHOD *mtd) : m_ssl_type(BIO_new(mtd), BIO_free_all) {}
+  explicit SSLBio(const BIO_METHOD *mtd) : m_ssl_type(BIO_new(mtd)) {}
+  explicit SSLBio(BIO *bio) : m_ssl_type(bio) {}
 
 public:
   SSLBio() = delete;
-  SSLBio(SSLBio &&) noexcept = default;
-  SSLBio(const SSLBio &) = default;
-  auto operator=(SSLBio &&) noexcept -> SSLBio & = default;
-  auto operator=(const SSLBio &) -> SSLBio & = default;
-  explicit SSLBio(BIO *ptr,
-                  std::function<void(BIO *)> free_fn = BIO_free_all)
-      : m_ssl_type(ptr, free_fn) {}
-  ~SSLBio() = default;
+  SSLBio(SSLBio&& bio) noexcept;
+  SSLBio(const SSLBio& bio);
+  auto operator=(SSLBio&& bio) noexcept -> SSLBio&;
+  auto operator=(const SSLBio& bio) -> SSLBio&;
+  ~SSLBio();
 
-  static auto init(const BIO_METHOD *mtd = BIO_s_mem()) -> SSLBio {
-    return SSLBio(mtd);
-  }
+  static auto init(const BIO_METHOD *mtd = BIO_s_mem()) -> SSLBio;
 
-  static auto open_file(const std::filesystem::path &&path) -> Expected<SSLBio> {
-    auto *bio_ptr = BIO_new_file(path.c_str(), "rb");
-    if (bio_ptr == nullptr) {
-      return Unexpected(SSLError(ErrorCode::IOError, "File not found"));
-    }
-    return {SSLBio(bio_ptr)};
-  }
+  static auto open_file(const std::filesystem::path &&path) -> Expected<SSLBio>;
 
-  auto as_ptr() const noexcept -> BIO* { return m_ssl_type.get(); }
+  auto as_ptr() const noexcept -> BIO*;
 
-  auto get_mem_ptr() -> Expected<std::string_view> {
-    BUF_MEM *bptr = BUF_MEM_new();
-    // Silence warnings
-    // BIO_get_mem_ptr(this->as_ptr(), &bptr);
-    BIO_ctrl(this->as_ptr(), 115, 0, reinterpret_cast<char *>(&bptr));
-    // BIO_set_close(this->as_ptr(), BIO_NOCLOSE);
-    BIO_ctrl(this->as_ptr(), 9, (0x00), nullptr);
-    return {{bptr->data, bptr->length}};
-  }
+  auto get_mem_ptr() const -> Expected<std::string_view>;
 
-  auto write_mem(const std::string_view&& buf) {
-    BIO_write(this->as_ptr(), buf.data(),
-              static_cast<int>(buf.length()));
-  }
+  auto write_mem(const std::string_view&& buf) -> void;
 };  // class SSLBio
 
 } // namespace openssl
