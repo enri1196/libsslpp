1b118b68be4783de01a7319b1daed2740171126b
Enri1196 <enrico1196@gmail.com>
2023-04-26 20:47:32 +0200

    evppkey openssl ref count

--- include/evp_pkey.hpp
+++ include/evp_pkey.hpp
@@ -1,7 +1,13 @@
 #pragma once
 
 #include <concepts>
+#include <iostream>
 #include <vector>
+
+#include "openssl/evp.h"
+#include "openssl/rsa.h"
+#include "openssl/ec.h"
+#include "openssl/pem.h"
 
 #include "bio.hpp"
 
@@ -15,55 +21,68 @@ class EcKey {};
 
 template<typename KeyType>
 requires std::same_as<Private, KeyType> || std::same_as<Public, KeyType>
-class EVPPkey {
+class LIBSSLPP_PUBLIC EVPPkey {
 private:
-  using SSLPtr = std::shared_ptr<EVP_PKEY>;
-  SSLPtr m_ssl_type;
+  EVP_PKEY* m_ssl_type;
 
-  EVPPkey() : m_ssl_type(EVP_PKEY_new(), EVP_PKEY_free) {}
+  EVPPkey() : m_ssl_type(EVP_PKEY_new()) {}
 
 public:
-  EVPPkey(const EVPPkey &) = default;
-  EVPPkey(EVPPkey &&) noexcept = default;
-  auto operator=(const EVPPkey &) -> EVPPkey& = default;
-  auto operator=(EVPPkey &&) noexcept -> EVPPkey& = default;
-  explicit EVPPkey(EVP_PKEY *ptr,
-                   std::function<void(EVP_PKEY *)> free_fn = EVP_PKEY_free)
-      : m_ssl_type(ptr, free_fn) {}
-  ~EVPPkey() = default;
+  EVPPkey(const EVPPkey& key) {
+    EVP_PKEY_up_ref(key.as_ptr());
+    m_ssl_type = key.m_ssl_type;
+  }
+  EVPPkey(EVPPkey&& key) noexcept {
+    m_ssl_type = key.m_ssl_type;
+    key = nullptr;
+  }
+  auto operator=(const EVPPkey& key) -> EVPPkey& {
+    if (this != &key) {
+      EVP_PKEY_up_ref(key.as_ptr());
+      m_ssl_type = key.m_ssl_type;
+    }
+    return *this;
+  }
+  auto operator=(EVPPkey&& key) noexcept -> EVPPkey& {
+    if (this != &key) {
+      m_ssl_type = key.m_ssl_type;
+      key.m_ssl_type = nullptr;
+    }
+    return *this;
+  }
+  EVPPkey(EVP_PKEY* key) : m_ssl_type(key) {}
+  ~EVPPkey() { EVP_PKEY_free(m_ssl_type); }
 
-  auto as_ptr() const noexcept -> EVP_PKEY* { return m_ssl_type.get(); }
+  auto as_ptr() const noexcept -> EVP_PKEY* { return m_ssl_type; }
 
   template<typename KeyAlgorithm>
   requires std::same_as<Rsa, KeyAlgorithm> && std::same_as<KeyType, Private>
   static auto generate(const std::int32_t bits = 2048) -> Expected<EVPPkey<Private>> {
-    auto evp = EVPPkey<Private>();
-    auto evp_ptr = evp.as_ptr();
+    auto evp = EVP_PKEY_new();
     auto evp_ctx = EVP_PKEY_CTX_new_id(EVP_PKEY_RSA, nullptr);
     EVP_PKEY_keygen_init(evp_ctx);
     if (EVP_PKEY_CTX_set_rsa_keygen_bits(evp_ctx, bits) <= 0) {
       EVP_PKEY_CTX_free(evp_ctx);
       return Unexpected(SSLError(ErrorCode::KeyGen));
     }
-    EVP_PKEY_keygen(evp_ctx, &evp_ptr);
+    EVP_PKEY_keygen(evp_ctx, &evp);
     EVP_PKEY_CTX_free(evp_ctx);
-    return {evp};
+    return {EVPPkey<Private>(evp)};
   }
 
   template<typename KeyAlgorithm>
   requires std::same_as<EcKey, KeyAlgorithm> && std::same_as<KeyType, Private>
   static auto generate(const std::int32_t nid = NID_secp521r1) -> Expected<EVPPkey<Private>> {
-    auto evp = EVPPkey<Private>();
-    auto evp_ptr = evp.as_ptr();
+    auto evp = EVP_PKEY_new();
     auto evp_ctx = EVP_PKEY_CTX_new_id(EVP_PKEY_EC, nullptr);
     EVP_PKEY_keygen_init(evp_ctx);
     if (EVP_PKEY_CTX_set_ec_paramgen_curve_nid(evp_ctx, nid) <= 0) {
       EVP_PKEY_CTX_free(evp_ctx);
       return Unexpected(SSLError(ErrorCode::KeyGen));
     }
-    EVP_PKEY_keygen(evp_ctx, &evp_ptr);
+    EVP_PKEY_keygen(evp_ctx, &evp);
     EVP_PKEY_CTX_free(evp_ctx);
-    return {evp};
+    return {EVPPkey<Private>(evp)};
   }
 
   auto get_public() const -> EVPPkey<Public>
@@ -74,7 +93,7 @@ public:
     EVP_PKEY_up_ref(this->as_ptr());
     PEM_write_bio_PUBKEY(bio.as_ptr(), evp_ptr);
     auto pub_key = PEM_read_bio_PUBKEY(bio.as_ptr(), &evp_ptr, nullptr, nullptr);
-    return EVPPkey<Public>(pub_key, [](EVP_PKEY*){});
+    return EVPPkey<Public>(pub_key);
   }
 
   auto sign(const std::vector<std::uint8_t>&& bytes) const -> std::vector<std::uint8_t>
