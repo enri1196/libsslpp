a4d8c899fae8242ac183df291c6ca46ce8ab5a44
Enri1196 <enrico1196@gmail.com>
2023-04-29 15:48:40 +0200

    x509 to cpp

--- src/x509/x509_certificate.cpp
+++ src/x509/x509_certificate.cpp
@@ -49,4 +49,99 @@ auto X509Certificate::from(const std::vector<std::uint8_t> &&cert_bytes)
   return {X509Certificate(cert)};
 }
 
+auto X509Certificate::from(const std::string_view &&cert_str)
+    -> Expected<X509Certificate> {
+  auto bio_str = SSLBio::init();
+  bio_str.write_mem(std::move(cert_str));
+  auto *cert = PEM_read_bio_X509(bio_str.as_ptr(), nullptr, nullptr, nullptr);
+  if (cert == nullptr) {
+    return Unexpected(SSLError(ErrorCode::ParseError));
+  }
+  return {X509Certificate(cert)};
+}
+
+auto X509Certificate::from(const std::filesystem::path &&file_path)
+    -> Expected<X509Certificate> {
+  const auto bio_file = TRY(SSLBio::open_file(std::move(file_path)));
+  auto cert = PEM_read_bio_X509(bio_file.as_ptr(), nullptr, nullptr, nullptr);
+  if (cert == nullptr) {
+    return Unexpected(SSLError(ErrorCode::ParseError));
+  }
+  return {X509Certificate(cert)};
+}
+
+auto X509Certificate::from(const SSLBio &&bio_cert) -> Expected<X509Certificate> {
+  auto cert = PEM_read_bio_X509(bio_cert.as_ptr(), nullptr, nullptr, nullptr);
+  if (cert == nullptr) {
+    return Unexpected(SSLError(ErrorCode::ParseError,
+                                "Couldn't parse certificate from bio"));
+  }
+  return {X509Certificate(cert)};
+}
+
+auto X509Certificate::to_string() const -> Expected<std::string_view> {
+  auto bio = SSLBio::init();
+  if (X509_print_ex(bio.as_ptr(), this->as_ptr(), XN_FLAG_SEP_CPLUS_SPC,
+                    X509_FLAG_COMPAT) == 0) {
+    return Unexpected(SSLError(ErrorCode::ParseError));
+  }
+  return bio.get_mem_ptr();
+}
+
+auto X509Certificate::not_before() const -> Expected<Asn1Time> {
+  auto time = X509_get_notBefore(this->as_ptr());
+  if (time == nullptr) {
+    return Unexpected(SSLError(ErrorCode::AccesError));
+  }
+  return {Asn1Time(time, [](ASN1_TIME*){})};
+}
+
+auto X509Certificate::not_after() const -> Expected<Asn1Time> {
+  auto time = X509_get_notAfter(this->as_ptr());
+  if (time == nullptr) {
+    return Unexpected(SSLError(ErrorCode::AccesError));
+  }
+  return {Asn1Time(time, [](ASN1_TIME*){})};
+}
+
+auto X509Certificate::serial_number() const -> Expected<Asn1Integer> {
+  ASN1_INTEGER *serial = X509_get_serialNumber(this->as_ptr());
+  if (serial == nullptr) {
+    return Unexpected(SSLError(ErrorCode::AccesError));
+  }
+  return {Asn1Integer(serial, [](ASN1_INTEGER*){})};
+}
+
+auto X509Certificate::public_key() const -> Expected<EVPPkey<Public>> {
+  auto pub_key = X509_get_pubkey(this->as_ptr());
+  if (pub_key == nullptr) {
+    return Unexpected(SSLError(ErrorCode::AccesError));
+  }
+  return {EVPPkey<Public>(pub_key)};
+}
+
+auto X509Certificate::issuer_name() const -> Expected<X509Name> {
+  auto issuer = X509_get_issuer_name(this->as_ptr());
+  if (issuer == nullptr) {
+    return Unexpected(SSLError(ErrorCode::AccesError));
+  }
+  return {X509Name(issuer, [](X509_NAME*){})};
+}
+
+auto X509Certificate::key_usage() -> Expected<KeyUsage> {
+  auto key_usage = X509_get_key_usage(this->as_ptr());
+  if (static_cast<KeyUsage>(key_usage) == KeyUsage::ABSENT) {
+    return Unexpected(SSLError(ErrorCode::AccesError));
+  }
+  return {static_cast<KeyUsage>(key_usage)};
+}
+
+auto X509Certificate::extended_key_usage() -> Expected<ExtendedKeyUsage> {
+  auto key_usage = X509_get_extended_key_usage(this->as_ptr());
+  if (static_cast<ExtendedKeyUsage>(key_usage) == ExtendedKeyUsage::ABSENT) {
+    return Unexpected(SSLError(ErrorCode::AccesError));
+  }
+  return {static_cast<ExtendedKeyUsage>(key_usage)};
+}
+
 }
